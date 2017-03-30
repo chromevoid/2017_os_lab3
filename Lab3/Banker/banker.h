65508c431bb5f4c1db9f406dfbae5d304b561f35
Chrome <chrome@127.179.233.68.naturalwireless.com>
2017-03-29 22:37:36 -0400

    created the project

---
+++ Lab3/Banker/banker.h
@@ -0,0 +1,8 @@
+//
+// Created by Chrome on 3/29/17.
+//
+
+#ifndef BANKER_BANKER_H
+#define BANKER_BANKER_H
+
+#endif //BANKER_BANKER_H
---
+++ Lab3/Banker/cmake-build-debug/CMakeFiles/Banker.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Banker.dir/main.cpp.o"
+  "Banker.pdb"
+  "Banker"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Banker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
